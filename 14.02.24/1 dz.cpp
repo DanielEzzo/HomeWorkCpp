11e1f2b482558e34f4841586a99f26084d4b3d9d
DanielEzzo <159913834+DanielEzzo@users.noreply.github.com>
2024-02-17 08:25:38 +0300

    Add files via upload

---
+++ 14.02.24/1 dz.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	//1 задание
+	cout << "Anacondaz\n\n";
+	cout << "Aloe solnce v zenite, zvenit budilnik - respawn" << endl;
+	cout << "Na dushe ne to Piter, ne to Pripyt', ne to Gastown" << endl;
+	cout << "Sgushyaitca v nebe dim ot koptyashih zavodov" << endl;
+
+	//2 задание
+	cout << "Every hunter wants\n\n";
+	cout << "\tKazdiy" << endl;
+	cout << "\t\tOhotnik" << endl;
+	cout << "\t\t\tZelaet" << endl;
+	cout << "\t\t\t\tZnat'" << endl;
+	cout << "\t\t\t\t\tGge ze" << endl;
+	cout << "\t\t\t\t\t\tSidit" << endl;
+	cout << "\t\t\t\t\t\t\tPhazan" << endl;
+
+	//3 задание
+	cout << "prodam PC\n\n";
+	cout << "Tel.: 8(988)-544-41-82" << endl;
+	cout << "Proc      " << char(179) << "ryzen12 8950 X 3D" << char(179) << endl;
+	cout << "Mat'      " << char(179) << "Super-puper plata" << char(179) << endl;
+	cout << "Nakopiteli" << char(179) << "99999999999999 PB" << char(179) << endl;
+	cout << "Kuler     " << char(179) << "ledyanaya svezost" << char(179) << endl;
+	cout << "Korpus    " << char(179) << "oochen' krutoy   " << char(179) << endl;
+	cout << "video     " << char(179) << "RTX 5090 TI Super" << char(179) << endl;
+	cout << "zena      " << char(179) << "99999999999rublei" << char(179) << endl;
+}
