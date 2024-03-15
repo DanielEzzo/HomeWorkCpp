3ebe2e8e6a558982c3cff907f580ca46642e154f
DanielEzzo <159913834+DanielEzzo@users.noreply.github.com>
2024-03-15 14:15:11 +0300

    Add files via upload

---
+++ 29.02.24/15.03.24/9 dz.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	const int SIZE1 = 5;
+	const int SIZE2 = 5;
+	int array[SIZE1][SIZE2]{}, chis;
+
+	cin >> chis;
+
+	for (int i = 0; i < SIZE1; i++) {
+		for (int j = 0; j < SIZE2; j++ , chis*=2) {
+			array[i][j] = chis;
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	*/
+
+	const int SIZE1 = 5;
+	const int SIZE2 = 5;
+	int array[SIZE1][SIZE2]{}, chis;
+
+	cin >> chis;
+
+	for (int i = 0; i < SIZE1; i++) {
+		for (int j = 0; j < SIZE2; j++, chis++) {
+			array[i][j] = chis;
+			cout << array[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+
+
